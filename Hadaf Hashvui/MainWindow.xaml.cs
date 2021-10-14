74726cc7ebf199656aa8bfb4026082abccb3c04c
israel372931 <israel372931@gmail.com>
2021-10-14 15:27:29 +0300

    14.10.2021

--- Hadaf Hashvui/MainWindow.xaml.cs
+++ Hadaf Hashvui/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Hadaf_Hashvui
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
     }
 }
