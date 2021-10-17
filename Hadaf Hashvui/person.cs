af0ee12f1072e7420ea64b9fd98894b3623c816e
israel372931 <israel372931@gmail.com>
2021-10-17 13:02:33 +0300

    17.10.2021

---
+++ Hadaf Hashvui/person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hadaf_Hashvui
+{
+    class person
+    {
+        private int id;
+        private int age;
+    }
+}
